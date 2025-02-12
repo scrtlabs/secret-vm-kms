88bc3b2aded47557ebcc146466620453ebba1bd9
DeDTihoN <t.grishechkin@gmail.com>
2025-02-12 22:20:51 +0200

    can be deployed

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,7 +5,7 @@ use std::result;
 use aes_siv::aead::generic_array::GenericArray;
 use aes_siv::siv::Aes128Siv;
 use log::warn;
-use x25519_dalek;
+// use x25519_dalek;
 
 #[derive(Debug)]
 pub enum CryptoError {
